df02f8792b04c880b744d679e6830657e70e8e49
joshuakamara <44464948+joshuakamara@users.noreply.github.com>
2018-11-23 15:01:37 +0000

    Linked MapViewController swift to story board
    made the class of the mapviewcontroller: UIViewController

--- Map RPG Game/MapViewController.swift
+++ Map RPG Game/MapViewController.swift
@@ -1,9 +1,8 @@
-//
-//  File.swift
-//  Map RPG Game
-//
-//  Created by Reuben Khoo (s5065758) on 23/11/2018.
-//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
-//
-
 import Foundation
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController{
+    
+    
+}
--- Map RPG Game/ViewController.swift
+++ Map RPG Game/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Map RPG Game
-//
-//  Created by Reuben Khoo (s5065758) on 23/11/2018.
-//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
