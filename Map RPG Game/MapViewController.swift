b5972df2ae58e6ab171be5f9e191d6dcb370fff7
Reuben Khoo (s5065758) <s5065758@mac38578.student.bournemouth.ac.uk>
2018-11-23 14:48:33 +0000

    Added MapViewController Swift File, Added 2nd View Controller
    haven't linked 2nd view controller to MapViewController file yet

---
+++ Map RPG Game/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Map RPG Game
+//
+//  Created by Reuben Khoo (s5065758) on 23/11/2018.
+//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
+//
+
+import Foundation
