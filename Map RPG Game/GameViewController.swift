fecf54135972178a07dd126b1961046dc4e2cada
Otis Kingsman (s5111442) <s5111442@mac38579.student.bournemouth.ac.uk>
2018-11-23 15:38:18 +0000

    Filled in GVC

--- Map RPG Game/GameViewController.swift
+++ Map RPG Game/GameViewController.swift
@@ -1,10 +1,3 @@
-//
-//  GameViewController.swift
-//  Map RPG Game
-//
-//  Created by Otis Kingsman (s5111442) on 23/11/2018.
-//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
-//
 import UIKit
 import Foundation
 
