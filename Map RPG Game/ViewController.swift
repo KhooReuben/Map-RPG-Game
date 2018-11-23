ad9bc7bf13db836cd900379eaa6dd5c31ee2e168
Reuben Khoo (s5065758) <s5065758@mac38578.student.bournemouth.ac.uk>
2018-11-23 14:10:52 +0000

    First file

---
+++ Map RPG Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map RPG Game
+//
+//  Created by Reuben Khoo (s5065758) on 23/11/2018.
+//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
