70159b88aa7a16c8915144a774a9414e0606b98f
Reuben Khoo (s5065758) <s5065758@mac42942.student.bournemouth.ac.uk>
2018-11-29 12:21:32 +0000

    Added Firebase to AppDelegate, Added new Swift File AppManager

---
+++ Map RPG Game/AppManager.swift
@@ -0,0 +1,17 @@
+//
+//  AppManager.swift
+//  Map RPG Game
+//
+//  Created by Reuben Khoo (s5065758) on 29/11/2018.
+//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class AppManager {
+    static let shared = AppManager ()
+    private let storyboard = UIStoryboard (name: "Main", bundle: nil)
+    private init () { }
+}
--- Map RPG Game/MapViewController.swift
+++ Map RPG Game/MapViewController.swift
@@ -1,10 +1,7 @@
-//
-//  File.swift
-//  Map RPG Game
-//
-//  Created by Reuben Khoo (s5065758) on 23/11/2018.
-//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
-//
+
 import MapKit
 import Foundation
+import Firebase
+
+
 
