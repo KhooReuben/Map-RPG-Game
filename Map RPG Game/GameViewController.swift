db33c43784a04e863bf5ebbb9567f507d6e1df04
Otis Kingsman (s5111442) <s5111442@mac42943.student.bournemouth.ac.uk>
2018-11-29 12:02:41 +0000

    added extra features (see details) to game
    attempts tracker
    labels of faction in control + location

--- Map RPG Game/GameViewController.swift
+++ Map RPG Game/GameViewController.swift
@@ -2,15 +2,24 @@ import UIKit
 import Foundation
 
 class GameViewController: UIViewController {
+    @IBOutlet weak var territoryLabel: UILabel!
+    @IBOutlet weak var factionLabel: UILabel!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var attemptsLabel: UILabel!
     var target: Float = 0
-    
+    var territory: String = ""
+    var attempts: Int = 3
+    // gather information from cocopods about who currently owns the territory
     
     override func viewDidLoad() {
         super.viewDidLoad()
         generateRandomNumber()
+        territoryLabel.text = "Bournemouth Uni"
+        attemptsLabel.text = "\(attempts)" + " attempts left"
+        factionLabel.text = "Team Instinct Territory"
+        //change colour of background
     }
 
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
@@ -20,15 +29,22 @@ class GameViewController: UIViewController {
     
     func generateRandomNumber() {
         target = Float.random(in: 0...100).rounded()
-        targetLabel.text = "Target = "+"\(target)"
+        targetLabel.text = "Target Value = "+"\(target)"
     }
     
     @IBAction func claimButton(_ sender: Any) {
         let score = slider.value.rounded() - target
         resultLabel.text = "\(score)"
     
-        if score > 5 {generateRandomNumber()}
-        else if score < -5 {generateRandomNumber()}
+        if score > 5 {generateRandomNumber()
+            attempts = attempts - 1
+            attemptsLabel.text = "\(attempts)" + " attempts left"
+        }
+        else if score < -5 {generateRandomNumber()
+            attempts = attempts - 1
+            attemptsLabel.text = "\(attempts)" + " attempts left"
+            
+        }
     }
     
 }
