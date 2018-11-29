9f7239bb8719fd3ebfc02e7e714a76c1d390fab1
Otis Kingsman (s5111442) <s5111442@mac42943.student.bournemouth.ac.uk>
2018-11-29 11:24:22 +0000

    added slider game

--- Map RPG Game/GameViewController.swift
+++ Map RPG Game/GameViewController.swift
@@ -2,5 +2,30 @@ import UIKit
 import Foundation
 
 class GameViewController: UIViewController {
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    var target: Float = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        generateRandomNumber()
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        generateRandomNumber()
+        slider.value = slider.minimumValue
+    }
+    
+    func generateRandomNumber() {
+        target = Float.random(in: 0...100).rounded()
+        targetLabel.text = "\(target)"
+    }
+    
+    @IBAction func claimButton(_ sender: Any) {
+        let score = slider.value.rounded() - target
+        resultLabel.text = "\(score)"
+    }
     
 }
