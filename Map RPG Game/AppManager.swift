2ae1f2adc3c931eb9c447369081c10aeef680c1d
Reuben Khoo (s5065758) <s5065758@mac42942.student.bournemouth.ac.uk>
2018-11-29 12:29:13 +0000

    Added var appContainer to AppManager swift file

--- Map RPG Game/AppManager.swift
+++ Map RPG Game/AppManager.swift
@@ -11,7 +11,17 @@ import UIKit
 import Firebase
 
 class AppManager {
+    
     static let shared = AppManager ()
     private let storyboard = UIStoryboard (name: "Main", bundle: nil)
     private init () { }
+    
+    var appContainer: AppContainerViewController
+    
+    func showApp () {
+    }
+    
+    func logout () {
+    }
+    
 }
