200e88a0bd85d66ae0260e09d7f796e636847827
Reuben Khoo (s5065758) <s5065758@mac42942.student.bournemouth.ac.uk>
2018-11-29 12:45:48 +0000

    edited AppManager, for 'showApp' and 'logout'
    added all code currently in 'showApp' and 'logout' funcs, as well as added a '!' to the var 'appContainer'

--- Map RPG Game/AppManager.swift
+++ Map RPG Game/AppManager.swift
@@ -16,12 +16,22 @@ class AppManager {
     private let storyboard = UIStoryboard (name: "Main", bundle: nil)
     private init () { }
     
-    var appContainer: AppContainerViewController
+    var appContainer: AppContainerViewController!
     
     func showApp () {
+    
+      var viewController: UIViewController
+      if Auth.auth().currentUser == nil {
+          viewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        } else {
+        viewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        }
+        appContainer.present(viewController, animated: true, completion: nil)
     }
     
     func logout () {
+        try! Auth.auth().signOut()
+        appContainer.presentedViewController?.dismiss(animated: true, completion: nil)
     }
     
 }
