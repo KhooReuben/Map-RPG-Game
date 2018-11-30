e07c7f5e656b29c5add62eceed4b74b91f5cf504
Otis Kingsman (s5111442) <s5111442@mac38579.student.bournemouth.ac.uk>
2018-11-30 15:04:19 +0000

    Added MapView Feautres

--- Map RPG Game/MapViewController.swift
+++ Map RPG Game/MapViewController.swift
@@ -1,9 +1,27 @@
-
 import MapKit
 import Foundation
 import Firebase
 
 class MapViewController : UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+//    let targetLocationManager = TargetLocationManager()
+    var endCoordinate = CLLocation()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.userTrackingMode = .follow
+        locationManager.requestAlwaysAuthorization()
+//        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+//        for location in targetLocationManager.locations {
+//            locationManager.startMonitoring(for: location.region)
+//            mapView.addAnnotation(location)
+//        }
+    }
 }
 
