e7f230d4ca09cf796724f550772fe11197d43a9c
Reuben Khoo (s5065758) <s5065758@mac42949.student.bournemouth.ac.uk>
2018-12-06 12:54:46 +0000

    added view controllers for AppDelegateViewController, LoginViewController and MapViewController

--- Map RPG Game/LoginViewController.swift
+++ Map RPG Game/LoginViewController.swift
@@ -13,12 +13,13 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
-    
     @IBOutlet weak var emailTextField: UITextField!
+    
     @IBOutlet weak var passwordTextField: UITextField!
     
+    
     @IBAction func login(_ sender: UIButton) {
-   
+        
         guard let email = emailTextField.text, let password = passwordTextField.text else { return }
         
         Auth.auth().signIn(withEmail: email, password: password) { user, error in
