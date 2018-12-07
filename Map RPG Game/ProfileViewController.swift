51d20ec56f5f969d7af1d44ef1677df71f71427a
Reuben Khoo (s5065758) <s5065758@mac38582.student.bournemouth.ac.uk>
2018-12-07 14:42:10 +0000

    Added sign in view controller and swift file, and profile view controller and swift file

---
+++ Map RPG Game/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Map RPG Game
+//
+//  Created by Reuben Khoo (s5065758) on 07/12/2018.
+//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
+//
+
+import Foundation
+
+class ProfileViewController: UIViewController {
+    
+}
---
+++ Map RPG Game/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  Map RPG Game
+//
+//  Created by Reuben Khoo (s5065758) on 07/12/2018.
+//  Copyright © 2018 Reuben Khoo (s5065758). All rights reserved.
+//
+
+import Foundation
+import Firebase
+import UIKit
+
+
+class SignInViewController: UIViewController {
+    
+}
