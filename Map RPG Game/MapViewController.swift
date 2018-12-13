ca4c657a452fc00068ea239e7dddc9465d00f47b
Reuben Khoo (s5065758) <s5065758@mac42943.student.bournemouth.ac.uk>
2018-12-13 13:00:09 +0000

    tried adding clicking icon action, might not work so be ready

--- Map RPG Game/MapViewController.swift
+++ Map RPG Game/MapViewController.swift
@@ -5,6 +5,8 @@ import Firebase
 
 class MapViewController : UIViewController {
 
+     var attackTerritory: Territory!
+    
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var TerritoryIdentifier: UILabel!
     
@@ -17,13 +19,34 @@ class MapViewController : UIViewController {
         
         mapView.userTrackingMode = .follow
         locationManager.requestAlwaysAuthorization()
-        locationManager.delegate = self
+  //     locationManager.delegate = self
         locationManager.startUpdatingLocation()
         
         for location in territoryManager.locations {
             locationManager.startMonitoring(for: location.region)
-            mapView.addAnnotation(location)
+           // mapView.addAnnotation(location)
             TerritoryIdentifier.text = "\(location.title ?? "--")" //default value = "--"
+            
+            
+            
+            func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+                guard let quote = view.annotation as? Territory else {return}
+                print(quote.title)
+                performSegue(withIdentifier: "Claim Territory", sender: quote)
+                //activates segue with the identifier from the current view controller's storyboard file - in this case the "info" button on the map view
+            }
+            
+            func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if segue.identifier == "Claim Territory" {
+                    do { let destination = segue.destination as! MapViewController
+                        destination.attackTerritory = (sender as! Territory)
+                        //this tells the segue to go to the second view controller
+                    }
+                }
+            
+            
+            
+            
         }
     }
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {     //when the segue happens
@@ -38,8 +61,10 @@ class MapViewController : UIViewController {
 //    func userSelectsLocation (data: GameViewController)     // sets the data sent accross to the delegate as a 'TargetLocation' group
 //}
 
-extension MapViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+
+//extension MapViewController: CLLocationManagerDelegate {
+//    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         //opens up territory for capture
     }
-}
+ // }
+//}
