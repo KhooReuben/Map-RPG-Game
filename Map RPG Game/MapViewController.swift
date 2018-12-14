c4328adbe15aff3e58b2d952b954c05c6cde0181
Joshua Kamara (s5103177) <s5103177@mac38578.student.bournemouth.ac.uk>
2018-12-14 15:52:28 +0000

    sigbart fixed

--- Map RPG Game/MapViewController.swift
+++ Map RPG Game/MapViewController.swift
@@ -3,7 +3,7 @@ import Foundation
 import CoreLocation
 import Firebase
 
-class MapViewController : UIViewController {
+class MapViewController: UIViewController {
 
      var attackTerritory: Territory!
     
@@ -18,14 +18,10 @@ class MapViewController : UIViewController {
         super.viewDidLoad()
         
         mapView.userTrackingMode = .follow
-        locationManager.requestAlwaysAuthorization()
   //     locationManager.delegate = self
         locationManager.startUpdatingLocation()
-        
         locationManager.requestAlwaysAuthorization()
         //makes it so the app gives a requestto use the user's location before using it directly
-        locationManager.delegate = self as! CLLocationManagerDelegate
-        locationManager.startUpdatingLocation()
         // gives updates on the user's location
         
         for location in territoryManager.locations {
@@ -35,48 +31,53 @@ class MapViewController : UIViewController {
             
         }
     }
-    
-    
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+}
+  
+    extension MapViewController: MKMapViewDelegate {
         
-        
-        
-        guard let annotation = annotation as? Territory else { return nil }
-        
-        let identifier = "marker"
-        var view: MKMarkerAnnotationView
-        
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-            as? MKMarkerAnnotationView {
-            dequeuedView.annotation = annotation
-            view = dequeuedView
-        } else {
-            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            view.canShowCallout = true
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        
-        return view
-    }
-    
-    
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        guard let quote = view.annotation as? Territory else {return}
-        print(quote.title)
-        performSegue(withIdentifier: "Claim Territory", sender: quote)
-        //activates segue with the identifier from the current view controller's storyboard file - in this case the "info" button on the map view
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "Claim Territory" {
-            do { let destination = segue.destination as! MapViewController
-                destination.attackTerritory = (sender as! Territory)
-                //this tells the segue to go to the second view controller
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            //guard let annotation = annotation as? Territory else { return nil }
+            
+            let identifier = "marker"
+            var view: MKMarkerAnnotationView
+            
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                as? MKMarkerAnnotationView {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+                view.image = UIImage(named: "redGraphic")
             }
+            
+            view.markerTintColor = .purple
+            
+            return view
         }
+        
     }
-    
+
+//
+//    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+//        guard let quote = view.annotation as? Territory else {return}
+//        print(quote.title)
+//        performSegue(withIdentifier: "Claim Territory", sender: quote)
+//        //activates segue with the identifier from the current view controller's storyboard file - in this case the "info" button on the map view
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "Claim Territory" {
+//            do { let destination = segue.destination as! MapViewController
+//                destination.attackTerritory = (sender as! Territory)
+//                //this tells the segue to go to the second view controller
+//            }
+//        }
+//    }
+//
     
 
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {     //when the segue happens
@@ -85,7 +86,6 @@ class MapViewController : UIViewController {
 //     //     destination.delegate = self
 //      }
 //   }
-}
 
 //protocol SendDelegate {
 //  func userSelectsLocation (data: GameViewController)     // sets the data sent accross to the delegate as a 'TargetLocation' group
