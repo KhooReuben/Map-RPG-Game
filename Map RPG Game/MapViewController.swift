838ac017c7abba1c8438b9afdafc721e147d0432
Reuben Khoo (s5065758) <s5065758@mac38579.student.bournemouth.ac.uk>
2018-12-14 14:49:44 +0000

    added more code to MapViewController, for the icon on map

--- Map RPG Game/MapViewController.swift
+++ Map RPG Game/MapViewController.swift
@@ -22,13 +22,44 @@ class MapViewController : UIViewController {
   //     locationManager.delegate = self
         locationManager.startUpdatingLocation()
         
+        locationManager.requestAlwaysAuthorization()
+        //makes it so the app gives a requestto use the user's location before using it directly
+        locationManager.delegate = self as! CLLocationManagerDelegate
+        locationManager.startUpdatingLocation()
+        // gives updates on the user's location
+        
         for location in territoryManager.locations {
             locationManager.startMonitoring(for: location.region)
             mapView.addAnnotation(location)
             TerritoryIdentifier.text = "\(location.title ?? "--")" //default value = "--"
             
         }
+    }
+    
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        
+        
+        guard let annotation = annotation as? Territory else { return nil }
+        
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
+        
+        return view
+    }
+    
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let quote = view.annotation as? Territory else {return}
@@ -45,6 +76,7 @@ class MapViewController : UIViewController {
             }
         }
     }
+    
     
 
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {     //when the segue happens
